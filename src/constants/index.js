b34aa724a1ac2697aacfbb1398a1c89ee679e72e
Yenduy Sanchez <yenduysanchez@Yenduys-MacBook-Pro.local>
2023-07-18 19:05:34 +0900

    adding testimonial and contact

--- src/constants/index.js
+++ src/constants/index.js
@@ -184,26 +184,26 @@ const experiences = [
 const testimonials = [
     {
         testimonial:
-            "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-        name: "Sara Lee",
+            "I thought it was impossible to make a website as beautiful as our product, but Yenduy proved me wrong.",
+        name: "Katherine P.",
         designation: "CFO",
-        company: "Acme Co",
-        image: "https://randomuser.me/api/portraits/women/4.jpg",
+        company: "Intel Co",
+        image: "https://randomuser.me/api/portraits/women/2.jpg",
     },
     {
         testimonial:
-            "I've never met a web developer who truly cares about their clients' success like Rick does.",
-        name: "Chris Brown",
-        designation: "COO",
-        company: "DEF Corp",
-        image: "https://randomuser.me/api/portraits/men/5.jpg",
+            "The end result exceeded my expectations, and I have received numerous compliments from my customers on the website's design and usability.",
+        name: "Josue Alderete",
+        designation: "CEO",
+        company: "Rent Now",
+        image: "https://randomuser.me/api/portraits/men/7.jpg",
     },
     {
         testimonial:
-            "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-        name: "Lisa Wang",
+            "After Yenduy optimized our website, our traffic increased by 50%. We can't thank him enough!",
+        name: "Lu Bai",
         designation: "CTO",
-        company: "456 Enterprises",
+        company: "Caribbean Tourism",
         image: "https://randomuser.me/api/portraits/women/6.jpg",
     },
 ];
